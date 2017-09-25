ff2450910bcd1d06e9c27c73a97550b740e0bc4c
Han F XetRA <zerqqr1@126.com>
2017-09-25 08:45:13 +0800

    Add files via upload

---
+++ helper.sh
@@ -0,0 +1,17 @@
+mkdir vpninstall && cd vpninstall
+apt-get update && apt-get -y install vim libssl-dev curl wget gcc && wget https://raw.githubusercontent.com/XetRAHF/setup-simple-pptp-vpn/master/setup.sh && sh setup.sh -u zxz -p 1234567890
+rm -rf setup.sh
+echo PPTP setup Finished
+wget https://raw.githubusercontent.com/XetRAHF/ShadowsocsAt8964/master/ipsec_1.sh
+sh ipsec_1.sh
+wget https://raw.githubusercontent.com/XetRAHF/ShadowsocsAt8964/master/ipsec_2.sh
+sh ipsec_2.sh
+cd ..
+rm -rf vpninstall
+echo IPSEC VPN Finished
+mkdir vpninstall && cd vpninstall
+apt-get update
+apt-get install -y wget
+wget https://raw.githubusercontent.com/XetRAHF/ShadowsocsAt8964/master/deploy.sh
+bash ./deploy.sh
+echo Your can now use it 
